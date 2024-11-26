7b32c468552436b08547da2f245ac95817fa9e1c
jevin10 <1owroller@proton.me>
2024-11-25 21:58:50 -0800

    Added banners and headings to boards

---
+++ src/lib/components/Heading/PageTitle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let {
+    title,
+    subtitle
+  }: {
+    title: string;
+    subtitle: string;
+  } = $props();
+</script>
+
+<div class="w-full flex flex-col items-center">
+  <div
+    class="font-bold text-[clamp(2.2rem,2.2rem+0.8vw,3rem)] tracking-[clamp(-0.5px,calc(-1px-0.2vw),-3px)] text-center w-full my-0"
+  >
+    {title}
+  </div>
+  <div class="text-xl my-0">
+    {subtitle}
+  </div>
+</div>
--- src/routes/(boards)/[board]/+layout.svelte
+++ src/routes/(boards)/[board]/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { getImageboardState } from '$lib/client/imageboard/Imageboard.svelte';
-  import { getPostsStore } from '$lib/stores/posts';
   import { getWsStore } from '$lib/stores/websocket';
   import { page } from '$app/stores';
   import { BoardService } from '$lib/client/imageboard/BoardService';
   import { setContext } from 'svelte';
   import type { BoardContext } from '$lib/types/imageboard';
+  import PageTitle from '$lib/components/Heading/PageTitle.svelte';
+  import Banner from '$lib/components/Heading/Banner.svelte';
 
   let { children } = $props();
 
   const wsStore = getWsStore();
-  const postsStore = getPostsStore();
   const imageboardState = getImageboardState();
 
   const boardContext: BoardContext = $state({
@@ -46,6 +46,32 @@
       }
     }
   });
+
+  const titleMap = {
+    void: {
+      title: '/v/ - void',
+      subtitle: 'Reality has always been virtual.'
+    },
+    seams: {
+      title: '/s/ - seams',
+      subtitle: 'The body is obsolete. Your garments are your shell.'
+    },
+    adventures: {
+      title: '/a/ - adventures',
+      subtitle: 'Acceleration without destination!'
+    }
+  } as const;
+
+  const pageTitle = $derived(titleMap[boardContext.name as keyof typeof titleMap]?.title ?? '');
+  const pageSubtitle = $derived(
+    titleMap[boardContext.name as keyof typeof titleMap]?.subtitle ?? ''
+  );
 </script>
 
+{#if boardContext.name}
+  <div class="mt-10">
+    <Banner />
+    <PageTitle title={pageTitle} subtitle={pageSubtitle} />
+  </div>
+{/if}
 {@render children()}

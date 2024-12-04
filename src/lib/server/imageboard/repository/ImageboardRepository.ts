97409b360736e1ae778128cefde8a13487c14a27
jevin10 <1owroller@proton.me>
2024-12-03 23:19:05 -0800

    Added uploadPost to imageboardRepository interface

--- src/lib/server/imageboard/repository/ImageboardRepository.ts
+++ src/lib/server/imageboard/repository/ImageboardRepository.ts
@@ -1,6 +1,8 @@
+import type { CreatePostPayload } from "$lib/types/ws/actions/schemas";
 import type { posts_new } from "@prisma/client";
 
 // Interacts with the database to get imageboard data
 export default interface ImageboardRepository {
   getContent(boardId: number | null, page: number, limit: number, threadId?: number): Promise<posts_new[]>;
+  uploadPost(params: CreatePostPayload): Promise<posts_new>;
 }
